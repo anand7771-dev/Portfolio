9f4adc42d930cd68465592a940368c008573226b
Anand Dev <your-ananddev7771@gmail.com>
2026-03-30 04:02:45 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,7 +43,7 @@ export default function About() {
             },
             {
               icon: FaMapMarkerAlt,
-              label: 'Bihar,India',
+              label: 'India',
             },
             {
               icon: FaCode,
